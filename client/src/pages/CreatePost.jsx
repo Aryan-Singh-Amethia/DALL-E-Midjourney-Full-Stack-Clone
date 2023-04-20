1ab3dae69275b97128fc1417ebfc2b7c59381de0
Aryan-Singh-Amethia <aryanamethia1998@gmail.com>
2023-04-20 23:06:18 +0530

    Commiting Full Image Generation Working Functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,17 +14,52 @@ function CreatePost() {
   const [generatingImg , setGeneratingImg] = useState(false);
   const [loading , setLoading] = useState(false);
 
-  const handleSubmit = () =>{
+  const generateImage = async ()=>{
+    if(form.prompt){
+      try{
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle',
+          {
+            method : 'POST',
+            headers : {
+              'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({prompt : form.prompt})
+          }
+        )
+        
+        const data = await response.json();
+
+        setForm({...form , photo : `data:image/jpeg;base64,${data.photo}`})
+      }catch(error){
+         alert(error); 
+      }finally{
+        setGeneratingImg(false);
+      }
+    }else{
+      alert('Please enter a prompt');
+    }
 
   };
 
-  const handleChange = (event) =>{
+  const handleSubmit = () =>{
+      
+  };
 
+  const handleChange = (e) =>{
+      setForm({
+        ...form ,
+        [e.target.name] : e.target.value
+      })
 
   };
 
   const handleSurpriseMe = () =>{
-
+      const randomPrompt = getRandomPrompt(form.prompt);
+      setForm({
+        ...form,
+        prompt : randomPrompt
+      })
   };
 
   return (
@@ -49,7 +84,7 @@ function CreatePost() {
                     handleChange={handleChange}
                     />
                   <FormField
-                    labelName='Your Name'
+                    labelName='Your Prompt'
                     type='text'
                     name='prompt'
                     placeholder='A plush toy robot sitting against a yellow wall'
@@ -74,8 +109,33 @@ function CreatePost() {
                        className="w-9/12 h-9/12 object-contain"
                      /> 
                   )
-                }     
+                }
+                {
+                  generatingImg && (
+                    <div className='absolute insert-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                      <Loader/>
+                    </div>
+                  )
+                }
               </div>       
+             </div>
+             <div className='mt-5 flex gap-5'>
+                <button
+                   type='button'
+                   onClick={generateImage}
+                   className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 px-2.5 text-center'>
+                  {generatingImg ? 'Generating...' : 'Generate'}
+                </button>
+             </div>
+             <div className='mt-10'>
+               <p className='mt-2 text-[#666e75] text-[14px]'>
+                Once you have created the image , you can share it with others in teh community.
+               </p>
+               <button
+                  type='submit'
+                  className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+                {loading ? 'Loading...' : 'Share with the Community'}
+               </button>
              </div>
         </form>
     </section>
